6cded0ad1b0217c66a7501c0be55deeb23e0e4de
karibusana <vincenzo.vecchio@email.it>
2014-02-02 11:19:18 +0000

    Permette di mostrare la data aggiornata negli articoli se ad esempio vengono rivisionati

---
+++ Modifiche-articolo-genesis.php
@@ -0,0 +1,25 @@
+<?php
+
+//* Permette di mostrare la data aggiornata negli articoli se ad esempio vengono rivisionati
+
+add_filter( 'genesis_post_info', 'custom_post_info' );
+
+function custom_post_info( $post_info ) {
+    $u_time = get_the_time( 'U' ); 
+	$u_modified_time = get_the_modified_time( 'U' ); 
+
+	if ( $u_modified_time >= $u_time + 86400 ) { 
+		$post_date = get_the_modified_time( 'j F, Y' ); 
+		$post_info =  __( 'Aggiornato', 'genesis' ) . ' [post_author_posts_link] ' . __( 'il ', 'genesis' ) . $post_date .' [post_comments] [post_edit]';
+		return $post_info; 
+	}
+	else { 
+		$post_date = get_the_time( 'j F, Y' );
+		$post_info =  __( 'Da', 'genesis' ) . ' [post_author_posts_link] ' . __( 'il ', 'genesis' ) . $post_date .' [post_comments] [post_edit]';
+		return $post_info;
+		 
+	}
+	
+}
+
+?>
